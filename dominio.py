7a145fb3a52d929b187055138b2c57cdddefbd75
crltsnch <crltsnch@myuax.com>
2024-04-15 17:09:40 +0200

    paraview

--- dominio.py
+++ dominio.py
@@ -5,4 +5,7 @@ def generar_dominio(dimensiones):
     dominio = np.zeros(dimensiones)
     return dominio
 
-
+def exportar_para_paraview(presion, desplazamiento, nombre_archivo='resultados_paraview.csv'):
+    df = pd.DataFrame({'Presion': presion, 'Desplazamiento': desplazamiento})
+    df.to_csv(nombre_archivo, index=False)
+    print(f"Datos exportados a '{nombre_archivo}' para Paraview.")
